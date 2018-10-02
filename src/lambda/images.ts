6c7ea99d341810ea618afed11a9f6cbe9b6ebe04
darkobits <joshua@frontlawn.net>
2018-10-02 01:13:33 -0700

    feat: Handle pagination.

--- src/lambda/images.ts
+++ src/lambda/images.ts
@@ -1,20 +1,70 @@
 import '@babel/polyfill';
+// import util from 'util';
 import {Context} from 'aws-lambda';
 import axios from 'axios';
+import parseLinkHeader from 'parse-link-header';
 import {buildResponse} from 'lib/utils';
+
+/**
+ * Unsplash ID of our collection.
+ */
+const COLLECTION_ID = '2742109';
+
+
+/**
+ * Maximum page size allowed by Unsplash.
+ */
+const MAX_PAGE_SIZE = 30;
 
 
 /**
  * Axios client bound to the Unsplash API.
  */
 const client = axios.create({
-  method: 'GET',
-  baseURL: 'https://api.unsplash.com',
   headers: {
     'Accept-Version': 'v1',
     'Authorization': `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
   }
 });
+
+// `collections/${COLLECTION_ID}/photos`
+
+/**
+ * Provided a URL for an API that returns "link" headers containing pagination
+ * hints, fetches all pages and returns the results.
+ */
+async function getAllPages(url: string): Promise<Array<any>> {
+  const page = await client({
+    method: 'GET',
+    url,
+    headers: {
+      'Accept-Version': 'v1',
+      'Authorization': `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
+    },
+    params: {
+      per_page: MAX_PAGE_SIZE
+    }
+  });
+
+  // Response has no pagination information; return data.
+  if (!page.headers.link) {
+    return page.data;
+  }
+
+  const parsedLinkHeader = parseLinkHeader(page.headers.link);
+
+  if (!parsedLinkHeader) {
+    throw new Error('Response contained a "link" header, but it was not parseable.');
+  }
+
+  // Last page has been reached.
+  if (!parsedLinkHeader.next) {
+    return page.data;
+  }
+
+  // Additional pages are available.
+  return page.data.concat(await getAllPages(parsedLinkHeader.next.url));
+}
 
 
 /**
@@ -22,16 +72,18 @@ const client = axios.create({
  */
 export async function handler(event: any, context: Context) { // tslint:disable-line no-unused
   try {
-    const collection = await client.get('collections/2742109/photos');
+    const collection = await getAllPages(`https://api.unsplash.com/collections/${COLLECTION_ID}/photos`);
 
-    if (!collection.data.length) {
+    if (!collection.length) {
       return buildResponse({statusCode: 500, body: 'The collection does not contain any photos.'});
     }
 
-    const photosInCollection = await Promise.all(collection.data.map(async (photo: any) => {
-      const res = await client.get(`photos/${photo.id}`);
+    const photosInCollection = await Promise.all(collection.map(async (photo: any) => {
+      const res = await client.get(`https://api.unsplash.com/photos/${photo.id}`);
       return res.data;
     }));
+
+    // console.log('HEADERS', util.inspect(collection.headers, {colors: true, depth: 20}));
 
     return buildResponse({body: photosInCollection});
   } catch (err) {
