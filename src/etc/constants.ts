ac29ba174c618ddc355395b817019e2d36043e00
darkobits <joshua@frontlawn.net>
2018-10-02 23:48:46 -0700

    misc: Increase cache TTL to 1 day.

--- src/etc/constants.ts
+++ src/etc/constants.ts
@@ -1,5 +1,5 @@
 /**
- * Netlify path where lambda functions are hosted.
+ * Netlify path where lambda functions are hosted. Provided by the DefinePlugin.
  */
 export const API_PREFIX = process.env.API_PREFIX;
 
@@ -8,13 +8,7 @@ export const API_PREFIX = process.env.API_PREFIX;
  * How long image collection data may persist in the cache before it must be
  * updated.
  */
-export const CACHE_TTL = '1 minute';
-
-
-/**
- * Drop/text shadow used in various places.
- */
-export const shadow = `0px 1px 5px rgba(0, 0, 0, 0.1)`;
+export const CACHE_TTL = '1 day';
 
 
 /**
