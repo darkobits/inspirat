be5f631a21ca9bcb837b674e42f85d88d7d126f4
darkobits <joshua@frontlawn.net>
2018-10-05 23:05:33 -0700

    misc: Set new tab title to "New Tab".

--- src/lib/title.ts
+++ src/lib/title.ts
@@ -2,6 +2,6 @@ export default function setTitle() {
   if (location.hostname.includes('frontlawn.net')) {
     document.title = 'Front Lawn';
   } else {
-    document.title = 'Inspirat';
+    document.title = 'New Tab';
   }
 }
