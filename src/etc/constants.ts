d998f3ec0320e76c82402a4bb5e03f7da66defa0
darkobits <joshua@frontlawn.net>
2018-10-05 23:08:56 -0700

    misc: Add several photo overrides.

--- src/etc/constants.ts
+++ src/etc/constants.ts
@@ -14,14 +14,77 @@ export const CACHE_TTL = '1 day';
 /**
  * Map of Unsplash photo IDs to background-position values.
  */
-export const BACKGROUND_POSITION_OVERRIDES: any = {
-  '-N_UwPdUs7E': 'center top',
-  '0NhhIdDUcqw': 'center 20%',
-  '1HkVV14d8Bg': 'right center',
-  '3TmLV0fLzfU': 'center 20%',
-  'FxU8KV7psMY': 'center top',
-  'o8Utw2ETExA': 'right center',
-  'pZXg_ObLOM4': 'center top',
-  'Zm2n2O7Fph4': 'center 75%',
-  'LgCj9qcrfhI': 'center top'
+export const BACKGROUND_RULE_OVERRIDES: any = {
+  'oMneOBYhJxY': {
+    maskAmount: '0.4'
+  },
+  'tKQ9NhQVCjM': {
+    maskAmount: '0.5'
+  },
+  'u0cSubf5F-E': {
+    backgroundPosition: 'center top'
+  },
+  'BM08uZ_Qu24': {
+    maskAmount: '0.4'
+  },
+  '-N_UwPdUs7E': {
+    backgroundPosition: 'center top'
+  },
+  '0NhhIdDUcqw': {
+    backgroundPosition: 'center 20%'
+  },
+  '1HkVV14d8Bg': {
+    backgroundPosition: 'right center'
+  },
+  '3TmLV0fLzfU': {
+    backgroundPosition: 'center 20%'
+  },
+  'FxU8KV7psMY': {
+    backgroundPosition: 'center top'
+  },
+  'o8Utw2ETExA': {
+    backgroundPosition: 'right center'
+  },
+  'pZXg_ObLOM4': {
+    backgroundPosition: 'center top'
+  },
+  'Zm2n2O7Fph4': {
+    backgroundPosition: 'center 75%'
+  },
+  'LgCj9qcrfhI': {
+    backgroundPosition: 'center top'
+  },
+  'kZ1zThg6G40': {
+    backgroundPosition: 'center top',
+    transform: 'scale(1.2) translateY(8.3333333%)'
+  },
+  'vpHCfunwDrQ': {
+    backgroundPosition: 'center top'
+  },
+  'c4XoMGxfsVU': {
+    backgroundPosition: 'center top'
+  },
+  '_WuPjE-MPHo': {
+    backgroundPosition: 'center top',
+    transform: 'scale(1.1)'
+  },
+  '_QoAuZGAoPY': {
+    maskAmount: '0.32'
+  },
+  'r7GOO6M2TmU': {
+    backgroundPosition: 'center 20%'
+  },
+  '9qvZSH_NOQs': {
+    backgroundPosition: 'center top'
+  },
+  '3bpKvzknix0': {
+    maskAmount: '0.4'
+  },
+  'qpemSW6_1Z0': {
+    backgroundPosition: 'center top',
+    transform: 'scale(1.2) translateY(8.3333333%)'
+  },
+  'KqVHRmHVwwM': {
+    maskAmount: '0.32'
+  }
 };
