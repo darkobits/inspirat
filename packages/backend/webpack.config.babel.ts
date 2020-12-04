c72b0284de5b20e01243d397ef07f9a147cb3b25
darkobits <joshua+github@frontlawn.net>
2020-12-04 01:43:13 -0800

    refactor(backend): Use Webpack Serverless preset.

--- packages/backend/webpack.config.babel.ts
+++ packages/backend/webpack.config.babel.ts
@@ -1,91 +1,15 @@
-import path from 'path';
+import { webpackServerless } from '@darkobits/tsx';
+import { gitDescribe } from '@darkobits/tsx/lib/utils';
 
-import execa from 'execa';
-import webpack from 'webpack';
-import serverlessWebpack from 'serverless-webpack';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { EnvironmentPlugin } from 'webpack';
 
 
-// ----- Backend Webpack Configuration -----------------------------------------
-
-async function main(): Promise<webpack.Configuration> {
-  const config: webpack.Configuration = {entry: {}};
-  config.module = {rules: []};
-  config.plugins = [];
-
-  config.mode = serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production';
-
-  const gitVersion = (await execa('git', ['describe'])).stdout;
-
-
-  // ----- Entry / Output ------------------------------------------------------
-
-  // Use Serverless config to determine our entry-points.
-  config.entry = serverlessWebpack.lib.entries;
-
-  config.target = 'node';
-
-  config.output = {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js'
-  };
-
-
-  // ----- Loaders -------------------------------------------------------------
-
-  // TypeScript & JavaScript files.
-  config.module.rules.push({
-    test: /\.(ts|tsx|js|jsx)$/,
-    exclude: /node_modules/,
-    use: [
-      {loader: 'babel-loader'}
-    ]
-  });
-
-
-  // ----- Module Resolution ---------------------------------------------------
-
-  config.resolve = {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-  };
-
-
-  // ----- Plugins -------------------------------------------------------------
-
-  config.plugins.push(new webpack.DefinePlugin({
-    'process.env.GIT_VERSION': JSON.stringify(gitVersion),
-    'process.env.BUILD_TIMESTAMP': JSON.stringify(new Date().toISOString())
+const config = webpackServerless(({ config }) => {
+  config.plugins.push(new EnvironmentPlugin({
+    GIT_VERSION: gitDescribe(),
+    BUILD_TIMESTAMP: new Date().toISOString()
   }));
-
-  if (config.mode === 'production') {
-    config.plugins.push(new ForkTsCheckerWebpackPlugin({
-      eslint: {
-        enabled: true,
-        // Be very careful when changing this; do a smoke test to produce a
-        // failing build to be sure nothing broke.
-        files: './src/**/*'
-      },
-      typescript: {
-        enabled: true,
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true
-        }
-      }
-    }));
-  }
+});
 
 
-  // ----- Misc ----------------------------------------------------------------
-
-  config.devtool = config.mode === 'development' ? '#inline-source-map' : undefined;
-
-  config.stats = 'minimal';
-
-
-  return config;
-}
-
-
-module.exports = main();
+module.exports = config({}, {});
