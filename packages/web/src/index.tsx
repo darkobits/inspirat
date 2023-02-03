28008688f68d9ba293446784d63abddad44db62c
Joshua <joshua+github@frontlawn.net>
2023-02-02 23:32:03 -0800

    feat: Add PWA support.

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -6,6 +6,7 @@ import App from 'components/App';
 import 'animate.css/animate.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'etc/global-styles.css';
+import 'etc/service-worker';
 
 render('#root', <App />);
 console.debug(`Version ${import.meta.env.GIT_DESC}.`);
