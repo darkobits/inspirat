e30a30cb696e341ce885eedbee519a3bbced7381
Joshua <joshua+github@frontlawn.net>
2024-06-06 17:17:37 -0700

    refactor: Storage `createInstance` uses default options.

--- src/web/lib/storage.ts
+++ src/web/lib/storage.ts
@@ -1,12 +1,23 @@
-import localforage from 'localforage';
+import LocalForage from 'localforage';
 
+type LocalForageOptions = Parameters<typeof LocalForage['createInstance']>[0];
 
-const storage = localforage.createInstance({
-  driver: localforage.LOCALSTORAGE,
+const defaultOptions: LocalForageOptions = {
+  driver: LocalForage.LOCALSTORAGE,
   name: 'inspirat',
   version: 1,
   description: 'Inspirat'
-});
+};
 
+const storage = LocalForage.createInstance(defaultOptions);
+
+const originalCreateInstance = storage.createInstance.bind(storage);
+
+storage.createInstance = (options: LocalForageOptions) => {
+  return Reflect.apply(originalCreateInstance, storage, [{
+    ...defaultOptions,
+    ...options
+  }]);
+};
 
 export default storage;
